5b09419e1d81a0437a4996cabf91c8e5be4479ac
AA Student <appacademy@Mini-18.local>
2022-11-15 15:54:03 -0800

    Finish views hopefully

--- music_app/app/controllers/application_controller.rb
+++ music_app/app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
-    # skip_before_action :verify_authenticity_token
-    helper_method :logged_in?, :current_user
+    skip_before_action :verify_authenticity_token
+    helper_method :logged_in?, :current_user, :login(user)
     
     def current_user
+        debugger
         @current_user ||= User.find_by(session_token: session[:session_token])
     end
 
