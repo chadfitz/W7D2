58a27f6aaf91cf87157191b23a897d3da2570654
AA Student <appacademy@Mini-18.local>
2022-11-15 13:41:30 -0800

    Add Spire,CRRLLL

---
+++ music_app/app/models/user.rb
@@ -0,0 +1,45 @@
+class User < ApplicationRecord
+    validates :email, :password_digest, :session_token, presence: true
+    validates :email, :session_token, uniqueness: true
+    validates :password, length:{minimum: 6}, allow_nil: true
+
+    attr_reader :password
+
+    before_validation :ensure_session_token
+
+    def self.find_by_credentials(email, password)
+        @user = User.find_by(email: email)
+        if user && user.is_password?(password)
+            return user
+        else
+            return nil
+        end
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    def reset_session_token
+        self.session_token = generate_session_token
+        self.save!
+        self.session_token
+    end
+
+    def ensure_session_token
+        self.session_token ||= generate_session_token
+    end
+
+    def generate_session_token
+        token = SecureRandom::urlsafe_base64
+        while User.exists?(session_token: token)
+            token = SecureRandom::urlsafe_base64
+        end
+        token
+    end
+end
